28661f738be8c03a337f1a57481f38f67b644afd
Camila Marcial Martinez <marcialcamila8@gmail.com>
2025-11-11 14:08:28 -0600

    funcion del boton de eliminar ya esta

--- src/app/pages/tableMovies/tableMovies.ts
+++ src/app/pages/tableMovies/tableMovies.ts
@@ -1,12 +1,10 @@
-import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { Router } from '@angular/router';
 import { MovieService } from '../../services/movie-service';
-import { Movies } from '../movies/movies';
 import { Movie } from '../interfaces/movie.interface';
 
 @Component({
   selector: 'app-table-movies',
-  imports: [],
   templateUrl: './tableMovies.html',
   styles: `
     :host {
@@ -16,23 +14,43 @@ import { Movie } from '../interfaces/movie.interface';
 })
 export class TableMovies {
   movies: Movie[] = [];
+
   private router = inject(Router);
   private movieService = inject(MovieService);
-  agregarPelicula() {
-    this.router.navigate(['/add-movie']);
-  }
+
   constructor() {
     this.getMovies();
   }
+
   getMovies() {
     this.movieService.getmovies().subscribe({
       next: (pelis) => {
-        console.log('cd', pelis);
         this.movies = pelis;
       },
       error: (error) => {
-        console.error(error);
+        console.error('Error al obtener películas:', error);
       },
     });
+  }
+
+  agregarPelicula() {
+    this.router.navigate(['/add-movie']);
+  }
+
+  eliminarPelicula(id: number) {
+    if (confirm('¿Seguro que deseas eliminar esta película?')) {
+      this.movieService.deleteMovie(id).subscribe({
+        next: () => {
+          this.movies = this.movies.filter((movie) => movie.id !== id);
+          alert('Película eliminada correctamente.');
+        },
+        error: (error) => {
+          console.error('Error al eliminar la película:', error);
+          alert(
+            `No se pudo eliminar la película:\nStatus: ${error.status}\nMensaje: ${error.message}`
+          );
+        },
+      });
+    }
   }
 }
--- src/app/services/movie-service.ts
+++ src/app/services/movie-service.ts
@@ -10,10 +10,16 @@ import { Movie } from '../pages/interfaces/movie.interface';
 export class MovieService {
   private url = environment.baseUrl;
   private http = inject(HttpClient);
+
   getmovies() {
     return this.http.get<Movie[]>(`${this.url}movie`);
   }
+
   postMovie(body: Movies) {
     return this.http.post<Movie>(`${this.url}movie`, body);
+  }
+
+  deleteMovie(id: number) {
+    return this.http.delete(`${this.url}movie/${id}`, { responseType: 'text' });
   }
 }
